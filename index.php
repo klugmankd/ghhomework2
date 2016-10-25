70dc8682fc8d6c1b2fe0a5a6ea355f47813236be
exnaton18 <exnaton18@gmail.com>
2016-10-25 13:05:29 +0300

    use Validation

--- index.php
+++ index.php
@@ -1,2 +1,73 @@
 <?php
     require __DIR__ . '/vendor/autoload.php';
+
+    use Respect\Validation\Validator as v;
+    include_once('view/index.php');
+
+    $msg = '';
+
+    $usernameValidator = v::alnum()->noWhitespace()->length(3, 15);
+    $firstNameValidator = v::alnum()->noWhitespace()->length(3, 40);
+    $passwordValidator = v::alnum()->noWhitespace()->length(8, 64);
+
+
+    if (isset($_GET['username'])) {
+        $username = $_GET['username'];
+        if ($usernameValidator->validate($username)) {
+            $valid = TRUE;
+        }
+        else {
+            echo $msg = "<div class='message'>Wrong name or empty name</div>";
+            $valid = FALSE;
+        }
+    }
+
+    if (isset($_GET['email'])) {
+        $email = $_GET['email'];
+        if (v::email()->validate($email)) {
+            $valid = TRUE;
+        }
+        else {
+            echo $msg = '<div class=\'message\'>Wrong email or empty</div>';
+            $valid = FALSE;
+        }
+    }
+
+    if (isset($_GET['firstName'])) {
+        $first_name = $_GET['firstName'];
+        if ($firstNameValidator->validate($first_name)) {
+            $valid = TRUE;
+        }
+        else {
+            echo $msg = '<div class=\'message\'>Wrong first name or empty</div>';
+            $valid = FALSE;
+        }
+    }
+
+    if (isset($_GET['password'])) {
+        $password = $_GET['password'];
+        if ($passwordValidator->validate($password)) {
+            $valid = TRUE;
+        }
+        else {
+            echo $msg = "<div class='message'>Wrong password or empty</div>";
+            $valid = FALSE;
+        }
+    }
+
+    if (isset($_GET['confirm'])) {
+        $confirm = $_GET['confirm'];
+        if ($passwordValidator->validate($confirm)) {
+            $valid = TRUE;
+        }
+        else {
+            echo $msg = "<div class='message'>Wrong confirm password or empty</div>";
+            $valid = FALSE;
+        }
+    }
+
+    if ($valid) {
+        echo "<div class='message'>All ok</div>";
+    } else {
+        echo "<div class='message'>All not ok</div>";
+    }
--- view/index.php
+++ view/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>GeekHub Homework</title>
-    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
     <div id="wrapper">
@@ -13,9 +13,9 @@
         <form method="get">
             <input type="text" name="username" class="field transition" placeholder="username">
             <input type="text" name="email" class="field transition" placeholder="email">
-            <input type="text" name="fullName" class="field transition" placeholder="full name">
+            <input type="text" name="firstName" class="field transition" placeholder="first name">
             <input type="password" name="password" class="field transition" placeholder="password">
-            <input type="password" name="password" class="field transition" placeholder="confirm password">
+            <input type="password" name="confirm" class="field transition" placeholder="confirm password">
             <input type="submit" name="submit" id="button" class="transition" value="Sign up">
         </form>
     </div>
